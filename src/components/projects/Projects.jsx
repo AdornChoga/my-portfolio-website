a19642548bb2bed6a8965cb9256ed19641dd814d
Adorn Choga <adornchoga@gmail.com>
2022-06-15 14:28:04 +0200

    Set layout and animation of project cards

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import classes from './Projects.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import './Projects.css';
 
 const Projects = () => (
   <div className="component-container">
     <h1 className="component-title">Projects</h1>
-    <div className={classes.cards}>
-      <div className={classes.card}>
-        <h3>Card 1</h3>
-      </div>
-    </div>
+    <Swiper
+      effect="coverflow"
+      grabCursor
+      centeredSlides
+      slidesPerView="auto"
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }}
+      pagination
+      loop
+      modules={[EffectCoverflow, Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <h3 className="text-center">Card 1</h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h3 className="text-center">Card 2</h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h3 className="text-center">Card 3</h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <h3 className="text-center">Card 4</h3>
+      </SwiperSlide>
+    </Swiper>
   </div>
 );
 
