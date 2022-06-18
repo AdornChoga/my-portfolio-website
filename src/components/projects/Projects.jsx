7abb901da1814346669eaebc5b333031ffcafce0
Adorn Choga <adornchoga@gmail.com>
2022-06-18 10:08:54 +0200

    Render projects using projects data store

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,66 +1,75 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {
-  EffectCoverflow, Pagination, Navigation, Keyboard,
-} from 'swiper';
+import { EffectCoverflow, Pagination, Navigation, Keyboard } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './Projects.css';
+import projects from './data/projects';
 
-const Projects = () => (
-  <div className="component-container">
-    <h1 className="component-title">Projects</h1>
-    <Swiper
-      effect="coverflow"
-      grabCursor
-      centeredSlides
-      slidesPerView="auto"
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      keyboard={{
-        enabled: true,
-      }}
-      navigation
-      loop
-      modules={[EffectCoverflow, Pagination, Navigation, Keyboard]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <div className="card">
-          <div className="card-front" />
-          <div className="card-back" />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="card">
-          <div className="card-front" />
-          <div className="card-back" />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="card">
-          <div className="card-front" />
-          <div className="card-back" />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="card">
-          <div className="card-front" />
-          <div className="card-back" />
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  </div>
-);
+const Projects = () => {
+  return (
+    <div className="component-container">
+      <h1 className="component-title">Projects</h1>
+      <Swiper
+        effect="coverflow"
+        grabCursor
+        centeredSlides
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        keyboard={{
+          enabled: true,
+        }}
+        navigation
+        loop
+        modules={[EffectCoverflow, Pagination, Navigation, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          {projects.map((project) => (
+            <div className="project-card" key={uuidv4()}>
+              <img
+                src={project.desktopImage}
+                alt={project.name}
+                className="card-front"
+              />
+              <div className="card-back">
+                <h2 className="project-name">{project.name}</h2>
+                <p className="description">{project.description}</p>
+                <div>
+                  <h3 className="text-center">Tech Used:</h3>
+                  <div className="tech-used">
+                    {project.techStack.map((tech) => (
+                      <img src={tech} alt="Javascript" key={uuidv4()} />
+                    ))}
+                  </div>
+                </div>
+                <div className="d-flex gap-3 links">
+                  <button type="button" className="btn btn-primary">
+                    <a href={project.liveLink}>Live App</a>
+                  </button>
+                  <button type="button" className="btn btn-primary">
+                    <a href={project.githubLink}>GitHub Repo</a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
 
 export default Projects;
---
+++ src/components/projects/data/projects.js
@@ -0,0 +1,23 @@
+import awesomeBooksDesktop from '../../../assets/images/awesome-books-desktop.png';
+import awesomeBooksMobile from '../../../assets/images/awesome-books-mobile.png';
+
+const projects = [
+  {
+    name: 'Awesome Books',
+    description:
+      'Awesome Books is a personal book library. It allows users to add'
+      + 'and remove books from their library by simply clicking add or'
+      + 'remove book buttons.',
+    mobileImage: awesomeBooksMobile,
+    desktopImage: awesomeBooksDesktop,
+    liveLink: 'https://adoring-blackwell-63e3e2.netlify.app/',
+    githubLink: 'https://github.com/AdornChoga/Awesome-Books-ES6',
+    techStack: [
+      'https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E',
+      'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+      'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
+    ],
+  },
+];
+
+export default projects;
